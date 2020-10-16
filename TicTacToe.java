9ad7f97c71590be143fc3de52fa20b9b206549e6
Miu <miu@Mius-MBP.hitronhub.home>
2020-10-16 09:28:39 -0700

    Changed

---
+++ TicTacToe.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class TicTacToe {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        char[] table = input char[3];
+
+        System.out.println("-----------------");
+
+
+    }
+
+    public static char getWinner(char[] arr) {
+
+        char x = 'X';
+        char y = 'Y';
+        boolean winner = false;
+
+        while (!winner) {
+            for (int row = 0; row < arr.length; row++) {
+                for (int column = 0; column < arr.length; column++) {
+                    
+                }
+            }
+        }
+
+
+
+    }
+}
